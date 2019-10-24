1ab3bf41c7cb19207121d2859fe58df53a3f1191
王玮 <306312851@qq.com>
2019-10-24 14:25:23 +0800

    5

--- Demo5/COpenGLWidget.cpp
+++ Demo5/COpenGLWidget.cpp
@@ -1,10 +1,44 @@
 #include "COpenGLWidget.h"
 #include <gl/GLU.h>
+#include <gl/freeglut.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 #pragma comment(lib,"opengl32.lib")
 #pragma comment(lib,"glu32.lib")
+#ifdef _DEBUG
+#pragma comment(lib,"freeglutd.lib")
+#else
+#pragma comment(lib,"freeglut.lib")
+#endif
+static GLfloat vertices[] = {
+	1,1,-1,
+	-1,1,-1,
+	-1,1,1,
+	1,1,1,
+	1,-1,1,
+	-1,-1,1,
+	-1,-1,-1,
+	1,-1,-1
+};
+static GLfloat colors[] = {
+	1,0,0,
+	0,1,0,
+	0,0,1,
+	1,1,0,
+	1,0,1,
+	0,1,1,
+	0.4,0.6,0.3,
+	0,0.8,0.4
+};
+static GLbyte indexs[] = {
+	0,1,2,3,
+	4,5,6,7,
+	0,3,4,7,
+	2,5,4,3,
+	2,1,6,5,
+	0,1,6,7
+};
 COpenGLWidget::COpenGLWidget(QWidget *parent)
 	: QOpenGLWidget(parent)
 {
@@ -13,6 +47,7 @@ COpenGLWidget::COpenGLWidget(QWidget *parent)
 COpenGLWidget::~COpenGLWidget()
 {
 }
+int _angle = 0;
 void COpenGLWidget::initializeGL() {
 	glShadeModel(GL_SMOOTH);
 	glClearColor(0, 0, 0, 0);
@@ -20,8 +55,14 @@ void COpenGLWidget::initializeGL() {
 	
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_DEPTH_TEST);
+
+	//glEnable(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, vertices);
+	glColorPointer(3, GL_FLOAT, 0, colors);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-	this->startTimer(1000);
+	this->startTimer(10);
 }
 void COpenGLWidget::resizeGL(int w, int h) {
 	if (h == 0) {
@@ -41,7 +82,7 @@ void COpenGLWidget::paintGL() {
 	//绘制圆锥
 	glPushMatrix();
 	glTranslatef(-1.5, 0.5, -7);
-	glRotatef(-45, 1, 1, 0);
+	glRotatef(_angle, 1, 1, 1);
 	//设置恒定着色
 	glShadeModel(GL_FLAT);
 	int m_Pivot = 0;
@@ -59,7 +100,6 @@ void COpenGLWidget::paintGL() {
 		//glVertex3f(std::sin(angle), 0, std::cos(angle));
 	}
 	glEnd();
-
 	m_Pivot = 0;
 	glBegin(GL_TRIANGLE_FAN);
 	glVertex2f(0, 0);
@@ -74,11 +114,39 @@ void COpenGLWidget::paintGL() {
 		glVertex3f(0.8*std::sin(angle), 0, 0.8*std::cos(angle));
 	}
 	glEnd();
-
 	glPopMatrix();
+	
+	//立方体
+	glPushMatrix();
+	glTranslatef(1.5, 1.0f, -7.0f);
+	glScalef(0.75, 0.75, 0.75);
+	glRotatef(_angle, 1, 1, 1);
+	glShadeModel(GL_SMOOTH);
+	/*glBegin(GL_QUADS);
+	for (int i = 0; i < 6; ++i) {
+		glArrayElement(indexs[i * 4 + 0]);
+		glArrayElement(indexs[i * 4 + 1]);
+		glArrayElement(indexs[i * 4 + 2]);
+		glArrayElement(indexs[i * 4 + 3]);
+	}
+	glEnd();*/
+	glDrawElements(GL_QUADS, sizeof(indexs)/sizeof(indexs[0]), GL_UNSIGNED_BYTE, indexs);
+	//glDrawArrays(GL_QUADS, 0, 8);
+	glPopMatrix();
+	
+	//glPushMatrix();
+	//glTranslatef(-1.5, -1.5, -7);
+	//glRotatef(_angle, 1, 1, 1);
+	//glColor3f(0.3, .7, .6);
+	////glutWireSphere(1.0, 12, 12);
+	////glutWireTeapot(1.0);
+	//glutWireTeapot(1.0);
+	//glPopMatrix();
 
 	glFlush();
 }
 void COpenGLWidget::timerEvent(QTimerEvent *event) {
 	this->update();
+	_angle += 1;
+	_angle %= 360;
 }
--- Demo5/main.cpp
+++ Demo5/main.cpp
@@ -1,9 +1,44 @@
 #include "Demo5.h"
 #include <QtWidgets/QApplication>
-
+#include <gl/freeglut.h>
+void RenderScenceCB() {
+	// 清空颜色缓存
+	glClear(GL_COLOR_BUFFER_BIT);
+	glTranslatef(-1.5, -1.5, -7);
+	//glRotatef(_angle, 1, 1, 1);
+	glColor3f(0.3, .7, .6);
+	//glutWireSphere(1.0, 12, 12);
+	//glutWireTeapot(1.0);
+	glutWireTeapot(1.0);
+	// 交换前后缓存
+	glutSwapBuffers();
+}
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	// 初始化GLUT
+	glutInit(&argc, argv);
+
+	//// 显示模式：双缓冲、RGBA
+	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	//// 窗口设置
+	//glutInitWindowSize(480, 320);      // 窗口尺寸
+	//glutInitWindowPosition(100, 100);  // 窗口位置
+	//glutCreateWindow("Tutorial 01");   // 窗口标题
+
+	//								   // 开始渲染
+	//glutDisplayFunc(RenderScenceCB);
+
+	//// 缓存清空后的颜色值
+	//glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//// 通知开始GLUT的内部循环
+	//glutMainLoop();
+
+	//return a.exec();
+	//QApplication a(argc, argv);
 	Demo5 w;
 	w.show();
 	return a.exec();

98b418ceaccd88402543acabb28142acad40fa94
王玮 <306312851@qq.com>
2019-10-28 20:15:23 +0800

    1

--- Demo4/COpenGLWidget.cpp
+++ Demo4/COpenGLWidget.cpp
@@ -39,6 +39,15 @@ static GLbyte indexs[] = {
 	2,1,6,5,
 	0,1,6,7
 };
+/**默认光源位置*/
+static float diffuseLight[] = {0.8,0.8,0.8,1.0};
+static float specularLight[] = { 1,1,1,1 };
+static float lightPosition[] = {0,0,5,1};
+/**红色光源*/
+static float lightPositionR[] = {0,0,5,1};
+static float diffuseLightR[] = {1,0,0,1};
+static float specularLightR[] = {1,0,0,1};
+
 COpenGLWidget::COpenGLWidget(QWidget *parent)
 	: QOpenGLWidget(parent)
 {
@@ -56,11 +65,29 @@ void COpenGLWidget::initializeGL() {
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_DEPTH_TEST);
 
+	//漫反射颜色
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+	//镜面反射颜色
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+	//光源位置
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuseLightR);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, specularLightR);
+	glLightfv(GL_LIGHT1, GL_POSITION, lightPositionR);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+
 	//glEnable(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
 	glVertexPointer(3, GL_FLOAT, 0, vertices);
 	glColorPointer(3, GL_FLOAT, 0, colors);
+
+	
+
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	this->startTimer(10);
 }
@@ -79,6 +106,13 @@ void COpenGLWidget::paintGL() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	
+	glPushMatrix();
+	glTranslatef(0, 0, -10);
+	glRotatef(_angle, 1, 1, 0);
+	glLightfv(GL_LIGHT1, GL_POSITION, lightPositionR);
+	glPopMatrix();
+
+
 	//绘制圆锥
 	glPushMatrix();
 	glTranslatef(-1.5, 0.5, -7);
@@ -122,6 +156,29 @@ void COpenGLWidget::paintGL() {
 	glScalef(0.75, 0.75, 0.75);
 	glRotatef(_angle, 1, 1, 1);
 	glShadeModel(GL_SMOOTH);
+
+	//无材质颜色
+	static float no_mat[] = { 0,0,0,1 };
+	//环境颜色
+	static float mat_ambient[] = { 0.7,0.7,0.7,1 };
+	//散射颜色
+	static float mat_diffuse[] = {0.2,0.5,0.8,1};
+	//镜面反射
+	static float mat_specular[] = {1,1,1,1};
+	static float no_shininess[] = { 0 };
+	//镜面反射指数
+	static float low_shininess[] = { 10 };
+	static float high_shininess[] = { 100 };
+	//发射光颜色
+	static float mat_emission[] = {0.3,0.2,0.3,0};
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, no_mat);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, low_shininess);
+	glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+
+
 	/*glBegin(GL_QUADS);
 	for (int i = 0; i < 6; ++i) {
 		glArrayElement(indexs[i * 4 + 0]);

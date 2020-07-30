6f4fe3a9c08b1d675647be4121e25925dad95ede
王玮 <306312851@qq.com>
2020-07-30 17:59:59 +0800

    1

--- Demo1/main.cpp
+++ Demo1/main.cpp
@@ -1,8 +1,14 @@
-#include "Demo1.h"
+﻿#include "Demo1.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
+	/*QSurfaceFormat format;
+	format.setDepthBufferSize(24);
+	format.setStencilBufferSize(8);
+	format.setVersion(4, 3);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	QSurfaceFormat::setDefaultFormat(format);*/
 	QApplication a(argc, argv);
 	Demo1 w;
 	w.show();
--- Demo6/main.cpp
+++ Demo6/main.cpp
@@ -1,7 +1,14 @@
-#include "Demo6.h"
+﻿#include "Demo6.h"
 #include <QtWidgets/QApplication>
+#include <QSurfaceFormat>
 int main(int argc, char *argv[])
 {
+	/*QSurfaceFormat format;
+	format.setDepthBufferSize(24);
+	format.setStencilBufferSize(8);
+	format.setVersion(4, 3);
+	format.setProfile(QSurfaceFormat::CoreProfile);
+	QSurfaceFormat::setDefaultFormat(format);*/
 	QApplication a(argc, argv);
 	Demo6 w;
 	w.show();
